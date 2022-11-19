67eff59fed2ec2c74c49bf5189ee1007b50632f3
Jehan <jehan.shah8@gmail.com>
2022-11-18 20:02:13 -0500

    record video

---
+++ src/record_video.py
@@ -0,0 +1,19 @@
+# crude script to record video from camera
+import os
+
+def record(): 
+	cmd = "nvgstcapture-1.0 \
+		--sensor-id=0 \
+		--mode=2 \
+		--video-res=6 \
+		--framerate=10 \
+		--orientation=2 \
+		--automate \
+		--capture-auto \
+		--file-name=test_video \
+		--capture-time=15 \
+		"
+	os.system(cmd)
+	
+if __name__ == "__main__":
+	record()
